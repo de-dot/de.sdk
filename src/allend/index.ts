f645942eb265741b4e04a115ec6bc4b7aba0fe8f
fabrice8 <fabrice.xyclone@gmail.com>
2025-09-30 14:45:33 +0000

    enhc: Refactor allend exports

--- src/allend/index.ts
+++ src/allend/index.ts
@@ -1,10 +1,14 @@
-import _MSI from './MSI'
-import _Utils from '../utils'
+import MSI from './MSI'
+import Utils from '../utils'
 
 import Order from './DClient/Order'
 import Event from './DClient/Event'
 import Client from './DClient/Client'
 
-export const MSI = _MSI
-export const Utils = _Utils
-export const DClient = { Client, Order, Event }
+const DClient = { Client, Order, Event }
+
+export default {
+  MSI,
+  Utils,
+  DClient
+}
